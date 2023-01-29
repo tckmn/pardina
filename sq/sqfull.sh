491b5b54351867b956af2beb17aafdb436fc957a
Andy Tockman <andy@tck.mn>
2023-01-29 17:46:37 -0500

    buffaleoh
    also sq

--- sq/sqfull.sh
+++ sq/sqfull.sh
@@ -5,3 +5,4 @@ clear
 grep -B999 -m1 XYZ "$fname" | head -n-3 | tac | grep -B999 -m1 '^ [0-9]\+:' | tac | tail -n+2 > before
 tac "$fname" | grep -A999 -m1 $'^ [0-9]\+:' | tail -n+2 | grep -B999 -m1 '^ [0-9]\+:' | tac | tail -n+2 > after
 grep '^ [0-9]\+: \+[^ ]' "$fname" | sort | uniq | sed -n '/XYZ/,${s/{ XYZ } //;p}' | tail -n+2 > answer
+rm "$fname"
